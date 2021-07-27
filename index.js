e30a65f3c3f01cef2d106957d2b1d486786ddc7f
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-07-27 14:32:17 -0500

    capture user input

--- index.js
+++ index.js
@@ -2,7 +2,130 @@
 const inquirer = require(`inquirer`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [`Project title: `, `Description: `, `Installation instructions: `, `Usage information: `, `Contribution guidelines: `, `Test instructions: `, `License for application (select one): `, `GitHub username: `, `Email address: `];
+
+const promptUser = () => {
+  console.log(`
+================
+README Generator
+================
+`);
+  return inquirer.prompt([
+    {
+      type: `input`,
+      name: `title`,
+      message: `Project title: `,
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log(`You need to enter a project title!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `description`,
+      message: `Description: `,
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log(`You need to enter a project description!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `installation`,
+      message: `Installation instructions: `,
+      validate: installationInput => {
+        if (installationInput) {
+          return true;
+        } else {
+          console.log(`You need to enter installation instructions!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `usage`,
+      message: `Usage information: `,
+      validate: usageInput => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log(`You need to enter usage information!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `contribution`,
+      message: `Contribution guidelines: `,
+      validate: contributionInput => {
+        if (contributionInput) {
+          return true;
+        } else {
+          console.log(`You need to enter contribution guidelines!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `test`,
+      message: `Test instructions: `,
+      validate: testInput => {
+        if (testInput) {
+          return true;
+        } else {
+          console.log(`You need to enter test instructions!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `list`,
+      name: `license`,
+      message: `License for application: `,
+      choices: [`GNU AGPLv3`, `GNU GPLv3`, `GNU LGPLv3`, `Mozilla Public License 2.0`, `Apache License 2.0`, `MIT License`, `Boost Software License 1.0`, `The Unlicense`]
+    },
+    {
+      type: `input`,
+      name: `github`,
+      message: `GitHub username: `,
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log(`You need to enter a GitHub username!`);
+          return false;
+        }
+      }
+    },
+    {
+      type: `input`,
+      name: `email`,
+      message: `Email address: `,
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log(`You need to enter an email address!`);
+          return false;
+        }
+      }
+    }
+  ]);
+};
+
+promptUser();
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
